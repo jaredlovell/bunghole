8bafaf494400a3f11301de8210ea98502c41df7b
Jared Lovell <jaredlovell@gmail.com>
2018-06-15 10:01:34 -0700

    fix cd

--- ghtrack/create_tours_db.sh
+++ ghtrack/create_tours_db.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-cd `basename $0`
+cd `dirname $0`
 
 die(){
 	echo $1
 	exit 1
 }
 
-psql -c "DROP DATABASE ght" -h localhost -U pgsql postgres || echo "no drop, maybe did not exist"
-psql -c "CREATE DATABASE ght" -h localhost -U pgsql postgres || die "could not create database"
-psql -f ./tours.sql -h localhost -U pgsql ght || die "could not create tables"
+psql -c "DROP DATABASE ght" -h localhost postgres || echo "no drop, maybe did not exist"
+psql -c "CREATE DATABASE ght" -h localhost postgres || die "could not create database"
+psql -f ./tours.sql -h localhost ght || die "could not create tables"
